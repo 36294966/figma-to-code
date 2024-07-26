56f6386f4ef84eb3071bf7405b42695fe2241063
Erastus <erastusngamau8@gmail.com>
2024-07-26 21:24:02 +0300

    updated version

--- figma-code/src/App.js
+++ figma-code/src/App.js
@@ -4,15 +4,27 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import AllAccess from "./Pages/AllAccess";
 import Browser from "./Pages/Browser";
+import heroImage from './assests/hero-home.webp'; // Adjust the path as needed
 
 const App = () => {
+  const bgStyle = {
+    backgroundImage: `url(${heroImage})`,
+  };
+
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/All-Access" element={<AllAccess />} />
-        <Route path="/Browser" element={<Browser />} />
-      </Routes>
+      <div
+        className="h-screen bg-cover bg-center flex flex-col"
+        style={bgStyle}
+      >
+        <Navbar />
+        <div className="flex-grow flex items-center justify-center">
+          <Routes>
+            <Route path="/All-Access" element={<AllAccess />} />
+            <Route path="/Browser" element={<Browser />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
--- figma-code/src/Pages/AllAccess.jsx
+++ figma-code/src/Pages/AllAccess.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const AllAccess = () => {
   return (
     <div className="bg-hero-home bg-cover bg-center h-screen flex items-center justify-center">
-      <h1 className="text-white text-3xl font-bold">Welcome to All Access</h1>
+      <h1 className="text-white text-3xl font-bold">9,498 curated design resources 
+        to speed up your creative workflow.</h1>
+        
     </div>
+   
   );
 };
 
